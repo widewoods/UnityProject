a2c810e4c41cf26aed1dc30946bc55e206be279e
Woods Hong <widewooz@gmail.com>
2020-10-31 00:25:45 +0900

    Create Loader.cs
    Loads objects into place and freezes the rigidbody.

---
+++ Platformer/Assets/Scripts/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(FreezeRigidbody());
+    }
+
+    IEnumerator FreezeRigidbody()
+    {
+        yield return new WaitForSeconds(0.5f);
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+}
