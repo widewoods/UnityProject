8d547b502e5ed4e54d8f3d9d9de2ccdf4611b3db
Woods Hong <widewooz@gmail.com>
2020-10-31 00:24:42 +0900

    Create dissolve shader graph and script

---
+++ Platformer/Assets/Scripts/Dissolve.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    Material material;
+
+    bool isDissolving = false;
+    float fade = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isDissolving = true;
+        }
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if(fade <= 0)
+            {
+                fade = 0f;
+                isDissolving = false;
+
+                Destroy(gameObject);
+            }
+
+            material.SetFloat("_Fade", fade);
+        }
+    }
+}
