e3dd5caf60e7e87f704c7aaa43c1363db6586e13
Woods Hong <widewooz@gmail.com>
2020-10-31 22:54:26 +0900

    Update BulletBehavior.cs
    Created a code to damage objects being collided with.

--- Platformer/Assets/Scripts/BulletBehavior.cs
+++ Platformer/Assets/Scripts/BulletBehavior.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+    public int bulletDamage = 20;
     public float bulletSpeed = 10f;
     Rigidbody2D rb;
 
     public GameObject bulletExplosionPrefab;
 
+    public EnemyScript enemy;
+    public PlayerScript player;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,22 +22,27 @@ public class BulletBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(transform.right * Time.deltaTime * bulletSpeed);
         rb.velocity = transform.right * bulletSpeed;
-
-        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            enemy = collision.gameObject.GetComponent<EnemyScript>();
+            enemy.TakeDamage(bulletDamage);
             Destroy(gameObject);
             Explode();
-            Destroy(collision.gameObject);
         }
 
-        else if (!collision.gameObject.CompareTag("Player"))
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject.GetComponent<PlayerScript>();
+            player.TakeDamage(bulletDamage);
+            Destroy(gameObject);
+            Explode();
+        }
+        else
         {
             Destroy(gameObject);
             Explode();
