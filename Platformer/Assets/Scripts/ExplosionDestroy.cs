d190590effbdf871787d384e9e374af9ef4b1555
Woods Hong <widewooz@gmail.com>
2020-10-31 22:49:12 +0900

    Add Explosion Scripts, Animation And Update ExplosionDestroy Script

--- Platformer/Assets/Scripts/ExplosionDestroy.cs
+++ Platformer/Assets/Scripts/ExplosionDestroy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ExplosionDestroy : MonoBehaviour
 {
+    SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 0.17f);
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(sprite.enabled == false)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Platformer/Assets/Scripts/ExplosiveScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveScript : MonoBehaviour
+{
+    public int explosionDmg;
+    public float explosionRad;
+
+    public GameObject explosionPrefab;
+    public EnemyScript enemy;
+    public PlayerScript player;
+
+    CircleCollider2D radius;
+
+    private void Start()
+    {
+        radius = GetComponent<CircleCollider2D>();
+        radius.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Explode(explosionRad);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemy = collision.gameObject.GetComponent<EnemyScript>();
+            enemy.TakeDamage(explosionDmg);
+        }
+
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject.GetComponent<PlayerScript>();
+            player.TakeDamage(explosionDmg);
+        }
+    }
+
+    void Explode(float explosionRadius)
+    {
+        radius.radius = explosionRadius;
+        radius.enabled = true;
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject, 0.21f);
+
+        FindObjectOfType<SoundManager>().Play("Explosion");
+    }
+}
