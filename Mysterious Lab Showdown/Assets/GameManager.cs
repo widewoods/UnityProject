435ae118d83c5663ebe2b6ac1a8325bf032aafff
Woods Hong <widewooz@gmail.com>
2020-11-03 23:02:22 +0900

    Change project name
    Changed project name from platformer to Mysterious Lab Showdown

---
+++ Mysterious Lab Showdown/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(EnemyScript.enemyCount == 0 && Input.GetKeyDown(KeyCode.C))
+        {
+            EndLevel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void EndLevel()
+    {
+        //Animation showing level cleared
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Mysterious Lab Showdown/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+
+
+public class CameraShake : MonoBehaviour
+{
+    CinemachineVirtualCamera vcam;
+    CinemachineBasicMultiChannelPerlin noise;
+
+    void Start()
+    {
+        vcam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
+        noise = vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    public IEnumerator Shake(float amplitudeGain, float frequencyGain, float duration)
+    {
+        noise.m_AmplitudeGain = amplitudeGain;
+        noise.m_FrequencyGain = frequencyGain;
+
+        yield return new WaitForSeconds(duration);
+
+        noise.m_AmplitudeGain = 0;
+        noise.m_FrequencyGain = 0;
+    }
+
+}
