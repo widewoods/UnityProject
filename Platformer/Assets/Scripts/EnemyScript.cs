712e46dee195c7f6c00ae0339ce9a247f5820591
Woods Hong <widewooz@gmail.com>
2020-11-03 23:03:52 +0900

    Revert "Change project name"
    This reverts commit 435ae118d83c5663ebe2b6ac1a8325bf032aafff.

---
+++ Platformer/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public GameObject player;
+    public GameObject gun;
+    public ParticleSystem particle;
+    public SpriteRenderer sprite;
+    public BoxCollider2D coll;
+    public Dissolve dissolve;
+
+    public static int enemyCount;
+    public float enemyHealth;
+
+    public string mode;
+
+    private Vector3 playerPos;
+    private Vector3 gunPos;
+
+    public bool isFlipped = false;
+    bool isDead = false;
+
+    public float bulletSpawnRate = 0.6f;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCount += 1;
+
+        particle = GetComponent<ParticleSystem>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+        dissolve = GetComponent<Dissolve>();
+
+        gun.transform.rotation = Quaternion.Euler(0, 0, 0);
+        if (isFlipped)
+        {
+            Flip();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            timer += Time.deltaTime;
+            if (timer >= bulletSpawnRate)
+            {
+                Shoot(mode);
+                timer = 0f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Die();
+            }
+
+        }
+
+    }
+
+    void Shoot(string mode)
+    {
+        if(mode == "follow")
+        {
+            gun.transform.LookAt(player.transform);
+            //playerPos = Camera.main.WorldToScreenPoint(player.transform.position);
+            //gunPos = Camera.main.WorldToScreenPoint(gun.transform.position);
+
+            //Vector2 direction;
+
+            //direction.x = playerPos.x - gunPos.x;
+            //direction.y = playerPos.y - gunPos.y;
+
+            //float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            //gun.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+
+        else if(mode == "straight")
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+        
+    }
+
+    void Flip()
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    void Die()
+    {
+        //sprite.enabled = false;
+        coll.enabled = false;
+        if (isFlipped)
+        {
+            Flip();
+        }
+        dissolve.StartDissolve();
+        //particle.Play();
+        Destroy(gameObject, 1.1f);
+        Destroy(gun);
+        isDead = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        enemyHealth -= damage;
+
+        if(enemyHealth <= 0)
+        {
+            Die();
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    public bool isFlipped;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMouse();
+
+        if (Mathf.Abs(transform.rotation.z) >= 90)
+        {
+            Flip();
+            Debug.Log("Flipped!");
+        }
+    }
+    void Flip()
+    {
+        isFlipped = true;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    void FollowMouse()
+    {
+        //Get mouse position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        //Get current position and compare with mouse position
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        //Calculate the angle and rotate
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
