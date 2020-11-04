7ce85fff4ba0e5c06126e4aee39c7f6886356063
Woods Hong <widewooz@gmail.com>
2020-11-04 22:08:55 +0900

    Revert "Revert "Change project name""
    This reverts commit 712e46dee195c7f6c00ae0339ce9a247f5820591.

---
+++ Mysterious Lab Showdown/Assets/Scripts/ExplosiveScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveScript : MonoBehaviour
+{
+    public int explosionDmg;
+    public float explosionRad;
+    public float explosionPower;
+
+    public GameObject explosionPrefab;
+    public EnemyScript enemy;
+    public PlayerScript player;
+    public GameObject platform;
+    public CameraShake cam;
+
+    private Collider2D[] colliders;
+
+    private void Start()
+    {
+        cam = FindObjectOfType<CameraShake>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            colliders = Physics2D.OverlapCircleAll(transform.position, explosionRad);
+            foreach(Collider2D col in colliders)
+            {
+                if (col.gameObject.CompareTag("Player"))
+                {
+                    player = col.gameObject.GetComponent<PlayerScript>();
+                    Explode(explosionPower, explosionRad, col.gameObject);
+                    player.TakeDamage(explosionDmg);
+                }
+                if (col.gameObject.CompareTag("Enemy"))
+                {
+                    enemy = col.gameObject.GetComponent<EnemyScript>();
+                    Explode(explosionPower, explosionRad, col.gameObject);
+                    enemy.TakeDamage(explosionDmg);
+                }
+
+                else
+                {
+                    Explode(0, 0, gameObject);
+                }
+            }
+        }
+    }
+
+    void Explode(float explosionPower, float explosionRadius, GameObject hitObject)
+    {
+        Vector3 hitObjectPos = hitObject.transform.position;
+        Vector3 barrelPos = transform.position;
+        Vector3 displacement = hitObjectPos - barrelPos;
+        Vector3 direction = displacement.normalized;
+        float distance = displacement.magnitude;
+
+        if(hitObject.GetComponent<Rigidbody2D>() != null)
+        {
+            if(distance <= explosionRadius)
+            {
+                hitObject.GetComponent<Rigidbody2D>().AddForce(direction * explosionPower, ForceMode2D.Impulse);
+                
+            }
+        }
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject, 0.05f);
+        FindObjectOfType<SoundManager>().Play("Explosion");
+    }
+}
---
+++ Mysterious Lab Showdown/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    public bool isFlipped;
+    public SpriteRenderer sprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMouse();
+
+    }
+
+    void FollowMouse()
+    {
+        //Get mouse position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        //Get current position and compare with mouse position
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+    }
+}
