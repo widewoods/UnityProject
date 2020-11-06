949b6867765a77b3c68b129549f8cb9cefedb001
Woods Hong <widewooz@gmail.com>
2020-11-06 22:44:31 +0900

    Create Player movement and rotation scripts

---
+++ 3D Project/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight = 20f;
+    public float yRot;
+    public float mouseSensitivity = 2f;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(Vector3.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(Vector3.forward * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+
+    }
+}
---
+++ 3D Project/Assets/RotateCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float xRot;
+    public float mouseSensitivity = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        xRot += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        transform.localEulerAngles = new Vector3(-xRot, transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+}
