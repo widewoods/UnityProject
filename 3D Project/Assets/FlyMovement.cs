e2e2a4cbc2221df18513538e4580994d9219a8c2
Woods Hong <widewooz@gmail.com>
2020-11-14 20:32:56 +0900

    Make Flying Scene In 3D Project

---
+++ 3D Project/Assets/FlyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyMovement : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float curSpeed;
+    public float maxSpeed;
+
+    public Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mousePos.z = 10f;
+
+        transform.Rotate(0f, (Input.mousePosition.x - Screen.width/2) * Time.deltaTime * rotateSpeed, 0f);
+        transform.Rotate(-(Input.mousePosition.y - Screen.height / 2) * Time.deltaTime * rotateSpeed, 0f, 0f);
+        
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            curSpeed += 0.1f;
+            rb.velocity = transform.forward * curSpeed;
+        }
+    }
+}
