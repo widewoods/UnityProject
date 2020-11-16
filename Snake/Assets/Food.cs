15d288e9d8e4d769ac6734a631f714ac5e7c8cee
Woods Hong <widewooz@gmail.com>
2020-11-16 12:41:18 +0900

    Create Food.cs

---
+++ Snake/Assets/Food.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    Snake snakeHead;
+    FoodSpawner foodSpawner;
+
+    private void Start()
+    {
+        snakeHead = FindObjectOfType<Snake>();
+        foodSpawner = FindObjectOfType<FoodSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == SnakeBody.snakeBodies[0].position)
+        {
+            snakeHead.GrowLength();
+            foodSpawner.SpawnFood();
+            Destroy(gameObject);
+        }
+    }
+}
