a8c39f79958ad6342bdc4527254760556574c2dc
Woods Hong <widewooz@gmail.com>
2020-11-17 18:55:46 +0900

    Create a script for highlighting the clicked jewel

---
+++ Bejeweled/Assets/Scripts/Highlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Jewel.firstClickedJewel != null)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            transform.position = Jewel.firstClickedJewel.position;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
