2a00f706242e4cfe5c9c5331003496ef93281483
Woods Hong <widewooz@gmail.com>
2020-11-18 11:21:23 +0900

    Make scripts for moving jewels around

---
+++ Bejeweled/Assets/Scripts/Jewel.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jewel : MonoBehaviour
+{
+    Vector2 mouseClickPosition;
+    Vector2 position;
+
+    public static Transform firstClickedJewel;
+    public static Transform secondClickedJewel;
+
+    public int type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        ResetClickedJewels();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SwapPlaces();
+            //Fall();
+        }
+    }
+
+    void SwapPlaces()
+    {
+        mouseClickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 displacement = (new Vector2(transform.position.x, transform.position.y) - mouseClickPosition);
+
+        if (displacement.magnitude < 0.5f)
+        {
+            if (firstClickedJewel == null && firstClickedJewel != transform)
+            {
+                firstClickedJewel = transform;
+            }
+            else if (firstClickedJewel != null && firstClickedJewel == transform)
+            {
+                ResetClickedJewels();
+            }
+            else if (firstClickedJewel != null && firstClickedJewel != transform)
+            {
+                secondClickedJewel = transform;
+                if((firstClickedJewel.position - secondClickedJewel.position).magnitude <= 1.1f)
+                {
+                    Vector3 direction = (secondClickedJewel.position - firstClickedJewel.position).normalized;
+                    StartCoroutine(AnimateTranslate(firstClickedJewel, direction));
+                    StartCoroutine(AnimateTranslate(secondClickedJewel, -direction));
+                    ResetClickedJewels();
+                    Debug.Log("Swapped");
+                }
+                else
+                {
+                    ResetClickedJewels();
+                    Debug.Log("Too Far");
+                }
+            }
+        }
+    }
+
+    void ResetClickedJewels()
+    {
+        firstClickedJewel = null;
+        secondClickedJewel = null;
+    }
+
+    IEnumerator AnimateTranslate(Transform transform, Vector2 direction)
+    {
+        for(int i = 0; i <= 9; i++)
+        {
+            transform.Translate(direction.normalized / 10);
+            yield return new WaitForSeconds(0.01f);
+        }
+        transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), 0);
+        position = transform.position;
+        //CheckSameType.positionGameObjectPair[position] = gameObject;
+        //Debug.Log(CheckSameType.positionGameObjectPair[position]);
+    }
+
+    //void Fall()
+    //{
+    //    if(transform.position.y > 0.1f)
+    //    {
+    //        if(!CheckSameType.positionGameObjectPair[position + Vector2.down])
+    //        {
+    //            Debug.Log("Fall");
+    //            StartCoroutine(AnimateTranslate(transform, Vector2.down));
+    //        }
+    //    }
+    //}
+}
--- Bejeweled/Assets/Scripts/JewelSpawner.cs
+++ Bejeweled/Assets/Scripts/JewelSpawner.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class JewelSpawner : MonoBehaviour
 {
-    public GameObject[] jewels = new GameObject[7];
+    public GameObject[] jewelsList = new GameObject[7];
     public GameObject jewelParent;
     public GameObject borderPrefab;
     public GameObject borderParent;
+    CheckSameType check;
 
     // Start is called before the first frame update
     void Start()
     {
         borderParent = new GameObject("Borders");
         jewelParent = new GameObject("Jewels");
+        check = FindObjectOfType<CheckSameType>();
         Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.I))
         {
             Initialize();
         }
@@ -29,13 +31,18 @@ public class JewelSpawner : MonoBehaviour
     void Initialize()
     {
         Destroy(jewelParent);
+        Destroy(borderParent);
         jewelParent = new GameObject("Jewels");
-        for(int x = 0; x < 9; x++)
+        borderParent = new GameObject("Borders");
+        for (int x = 0; x < 9; x++)
         {
             for(int y = 0; y < 9; y++)
             {
                 int randomIndex = Random.Range(0, 7);
-                Instantiate(jewels[randomIndex], new Vector2(x, y), Quaternion.identity, jewelParent.transform);
+                GameObject jewel = Instantiate(jewelsList[randomIndex], new Vector2(x, y), Quaternion.identity, jewelParent.transform);
+                jewel.GetComponent<Jewel>().type = randomIndex;
+                CheckSameType.positionGameObjectPair[new Vector2(x,y)] = jewel;
+
                 Instantiate(borderPrefab, new Vector2(x,y), Quaternion.identity, borderParent.transform);
             }
         }
