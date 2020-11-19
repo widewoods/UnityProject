1f19b39bd0bd18e49941d076707414e21f2dd917
Woods Hong <widewooz@gmail.com>
2020-11-19 09:08:07 +0900

    Create falling code

--- Bejeweled/Assets/Scripts/Jewel.cs
+++ Bejeweled/Assets/Scripts/Jewel.cs
@@ -7,14 +7,22 @@ public class Jewel : MonoBehaviour
     Vector2 mouseClickPosition;
     Vector2 position;
 
+    public Vector2 positionToRemove;
+
+    UpdatePositions updatePositions;
+
     public static Transform firstClickedJewel;
     public static Transform secondClickedJewel;
+
+    public static List<GameObject> temps = new List<GameObject>();
+    public GameObject tempPrefab;
 
     public int type;
 
     // Start is called before the first frame update
     void Start()
     {
+        updatePositions = gameObject.GetComponent<UpdatePositions>();
         position = transform.position;
         ResetClickedJewels();
     }
@@ -25,7 +33,14 @@ public class Jewel : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             SwapPlaces();
-            //Fall();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Fall();
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            RemoveAtPos(positionToRemove);
         }
     }
 
@@ -47,7 +62,7 @@ public class Jewel : MonoBehaviour
             else if (firstClickedJewel != null && firstClickedJewel != transform)
             {
                 secondClickedJewel = transform;
-                if((firstClickedJewel.position - secondClickedJewel.position).magnitude <= 1.1f)
+                if ((firstClickedJewel.position - secondClickedJewel.position).magnitude <= 1.1f)
                 {
                     Vector3 direction = (secondClickedJewel.position - firstClickedJewel.position).normalized;
                     StartCoroutine(AnimateTranslate(firstClickedJewel, direction));
@@ -72,26 +87,34 @@ public class Jewel : MonoBehaviour
 
     IEnumerator AnimateTranslate(Transform transform, Vector2 direction)
     {
-        for(int i = 0; i <= 9; i++)
+        for (int i = 0; i <= 9; i++)
         {
             transform.Translate(direction.normalized / 10);
             yield return new WaitForSeconds(0.01f);
         }
         transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), 0);
         position = transform.position;
-        //CheckSameType.positionGameObjectPair[position] = gameObject;
-        //Debug.Log(CheckSameType.positionGameObjectPair[position]);
     }
 
-    //void Fall()
-    //{
-    //    if(transform.position.y > 0.1f)
-    //    {
-    //        if(!CheckSameType.positionGameObjectPair[position + Vector2.down])
-    //        {
-    //            Debug.Log("Fall");
-    //            StartCoroutine(AnimateTranslate(transform, Vector2.down));
-    //        }
-    //    }
-    //}
+    public void Fall()
+    {
+        if (transform.position.y > 0)
+        {
+            if (CheckSameType.positionGameObjectPair[position + Vector2.down].CompareTag("Temp")) 
+            {
+                GameObject temp = Instantiate(tempPrefab, position, Quaternion.identity);
+                temps.Add(temp);
+                CheckSameType.positionGameObjectPair[position] = temp;
+                StartCoroutine(AnimateTranslate(transform, Vector2.down));
+            }
+        }
+    }
+
+    public void RemoveAtPos(Vector2 position)
+    {
+        GameObject temp = Instantiate(tempPrefab, position, Quaternion.identity);
+        temps.Add(temp);
+        Destroy(CheckSameType.positionGameObjectPair[position]);
+
+    }
 }
