3a896fd8b9cdfef58be6555a4f4cab1c4685751b
Woods Hong <widewooz@gmail.com>
2020-11-19 17:16:40 +0900

    Move function for removing jewels at a position from Jewel.cs to GameManager.cs

--- Bejeweled/Assets/Scripts/GameManager.cs
+++ Bejeweled/Assets/Scripts/GameManager.cs
@@ -4,45 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    static List<GameObject> jewels = new List<GameObject>();
-    CheckSameType checkSameType;
+    public GameObject tempPrefab;
+    public Vector2 position;
 
-    Vector2 position;
-
-    private void Update()
+    public static void RemoveAtPos(Vector2 position, GameObject tempPrefab)
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            UpdateAll();
-        }
-    }
-
-    public static void UpdateAll()
-    {
-        for (int x = 0; x < 9; x++)
-        {
-            for (int y = 0; y < 9; y++)
-            {
-                Vector2 position = new Vector2(x, y);
-                jewels.Add(CheckSameType.positionGameObjectPair[position]);
-            }
-        }
-
-        foreach(GameObject jewel in jewels)
-        {
-            if(jewel.TryGetComponent(out Jewel jewelComponent))
-            {
-                jewelComponent.Fall();
-            }
-        }
-
-        foreach (GameObject temp in Jewel.temps)
-        {
-            if (temp)
-            {
-                Destroy(temp);
-                CheckSameType.positionGameObjectPair[temp.transform.position] = null;
-            }
-        }
+        GameObject temp = Instantiate(tempPrefab, position, Quaternion.identity);
+        Destroy(CheckSameType.columns[Mathf.RoundToInt(position.x)][Mathf.RoundToInt(position.y)]);
+        CheckSameType.columns[Mathf.RoundToInt(position.x)][Mathf.RoundToInt(position.y)] = temp;
     }
 }
