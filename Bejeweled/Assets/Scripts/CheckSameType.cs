afd1f191578bbc4bd31ce3b6d4ac2ec3a6e282e2
Woods Hong <widewooz@gmail.com>
2020-11-20 10:43:08 +0900

    Change to restart checking whenever there is a match
    Checking from the bottom again when there is a new match/when jewels are swapped is faster than constantly having it run through.

--- Bejeweled/Assets/Scripts/CheckSameType.cs
+++ Bejeweled/Assets/Scripts/CheckSameType.cs
@@ -21,16 +21,7 @@ public class CheckSameType : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            for (int x = 0; x < 9; x++)
-            {
-                for (int y = 0; y < 9; y++)
-                {
-                    Debug.Log(columns[x][y]);
-                }
-            }
-        }
+
     }
 
     public IEnumerator Match(int row, int column, string direction)
@@ -39,17 +30,19 @@ public class CheckSameType : MonoBehaviour
         {
             for (int x = 0; x < 8; x++)
             {
-                if(columns[x][row].GetComponent<Jewel>().type == columns[x + 1][row].GetComponent<Jewel>().type)
+                if (columns[x][row].GetComponent<Jewel>().type == columns[x + 1][row].GetComponent<Jewel>().type)
                 {
                     horizontalMatchCount++;
                     if (horizontalMatchCount == 3)
                     {
+                        StopCoroutine(MatchAll());
                         for (int i = 1; i > -2; i--)
                         {
                             GameManager.RemoveAtPos(columns[x + i][row].transform.position, gameManager.tempPrefab);
-                            yield return null;
+                            yield return new WaitForEndOfFrame();
                         }
                         horizontalMatchCount = 1;
+                        StartCoroutine(MatchAll());
                     }
                 }
                 else
@@ -69,12 +62,14 @@ public class CheckSameType : MonoBehaviour
                     verticalMatchCount++;
                     if (verticalMatchCount == 3)
                     {
+                        StopCoroutine(MatchAll());
                         for (int i = 1; i > -2; i--)
                         {
                             GameManager.RemoveAtPos(columns[column][x + i].transform.position, gameManager.tempPrefab);
                             yield return new WaitForEndOfFrame();
                         }
                         verticalMatchCount = 1;
+                        StartCoroutine(MatchAll());
                     }
                 }
                 else
@@ -88,21 +83,33 @@ public class CheckSameType : MonoBehaviour
 
     public IEnumerator MatchAll()
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(0.2f);
-            for (int row = 0; row < 9; row++)
-            {
-                StartCoroutine(Match(row, column, "Horizontal"));
-                yield return new WaitForSeconds(0.01f);
-            }
+        //while (true)
+        //{
+        //    yield return new WaitForSeconds(0.2f);
+        //    for (int row = 0; row < 9; row++)
+        //    {
+        //        StartCoroutine(Match(row, column, "Horizontal"));
+        //        yield return null;
+        //    }
 
-            for (int column = 0; column < 9; column++)
-            {
-                StartCoroutine(Match(row, column, "Vertical"));
-                yield return new WaitForSeconds(0.01f);
-            }
+        //    for (int column = 0; column < 9; column++)
+        //    {
+        //        StartCoroutine(Match(row, column, "Vertical"));
+        //        yield return null;
+        //    }
+        //}
+        yield return new WaitForSeconds(0.2f);
+        for (int row = 0; row < 9; row++)
+        {
+            StartCoroutine(Match(row, column, "Horizontal"));
+            yield return new WaitForEndOfFrame();
+        }
+
+        for (int column = 0; column < 9; column++)
+        {
+            StartCoroutine(Match(row, column, "Vertical"));
+            yield return new WaitForEndOfFrame();
+
         }
     }
-
 }
