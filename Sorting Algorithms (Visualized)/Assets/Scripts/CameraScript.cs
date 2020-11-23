acc6832d7fa716b008b6615de3b117b5b5235f37
Woods Hong <widewooz@gmail.com>
2020-11-23 10:08:29 +0900

    Create Camera Script
    Camera adjusts to fit the size of the array

---
+++ Sorting Algorithms (Visualized)/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    int[] array;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        array = RandomArray.randomArray;
+        transform.position = new Vector3(array.Length/2 , array.Length/2 - 0.5f, -10);
+        Camera.main.orthographicSize = array.Length/2;
+    }
+}
