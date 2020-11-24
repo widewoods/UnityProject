0ab89f1bdb454a5c772d060b682cd09ee89847a7
Woods Hong <widewooz@gmail.com>
2020-11-24 17:38:46 +0900

    Create a node class

---
+++ Pathfinding Algorithms/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Vector2 position;
+    public int posX;
+    public int posY;
+
+    public bool visited;
+    public bool passable;
+
+    public int state;
+}
