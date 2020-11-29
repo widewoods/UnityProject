6ea6df0cd5d9cc46d0ade313d174eb30fa2935c5
Woods Hong <widewooz@gmail.com>
2020-11-30 08:47:11 +0900

    Create basic moves
    Movement and attack

---
+++ Dungeon RPG/Assets/Script/MeleeAttackScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Todo: Add inventory to hold multiple items
+
+public class MeleeAttackScript : MonoBehaviour
+{
+    public MeleeWeaponData weaponData;
+
+    private GameObject weapon;
+
+    public GameObject player;
+
+    public GameObject trail;
+
+    void Start()
+    {
+        trail.SetActive(false);
+        weapon = gameObject;
+        transform.parent = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Swing(weaponData.swingSpeed));
+            //Swing(weaponData.swingSpeed);
+        }
+    }
+
+    IEnumerator Swing(float swingSpeed)
+    {
+        trail.SetActive(true);
+
+        weapon.transform.localRotation = Quaternion.Euler(0f, 0f, -105f);
+
+        //Add swing animation
+        int i = 0;
+        while (i < 50)
+        {
+            weapon.transform.localRotation = Quaternion.Slerp(weapon.transform.localRotation, Quaternion.Euler(0f, 0f, 15f), Time.deltaTime * swingSpeed);
+            yield return new WaitForEndOfFrame();
+            i++;
+        }
+        yield return null;
+
+        weapon.transform.localRotation = Quaternion.Euler(0f, 0f, -45f);
+
+        trail.SetActive(false);
+    }
+}
---
+++ Dungeon RPG/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int horizontal = (int)Input.GetAxisRaw("Horizontal");
+        int vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if(Mathf.Abs(horizontal) == 1)
+        {
+            Vector2 dir = Vector2.right * horizontal;
+            transform.Translate(dir * Time.deltaTime * speed, Space.World);
+        }
+        if (Mathf.Abs(vertical) == 1)
+        {
+            Vector2 dir = Vector2.up * vertical;
+            transform.Translate(dir * Time.deltaTime * speed, Space.World);
+        }
+
+        RotateToMouse();
+    }
+
+    void RotateToMouse()
+    {
+        Vector2 mousePositon = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float x = mousePositon.x - transform.position.x;
+        float y = mousePositon.y - transform.position.y;
+
+        float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
