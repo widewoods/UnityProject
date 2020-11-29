48bc0a5228c564a955789eeb9aa5a60395544a6a
Woods Hong <widewooz@gmail.com>
2020-11-30 08:47:27 +0900

    Create weapon data scriptable object

---
+++ Dungeon RPG/Assets/Script/MeleeWeaponData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class MeleeWeaponData : ScriptableObject
+{
+    public int damage;
+
+    public float swingSpeed;
+
+    public float range;
+}
