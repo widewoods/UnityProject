8412899b09747183f18945498a6d956fafac5012
Woods Hong <widewooz@gmail.com>
2020-12-19 16:46:25 +0900

    Create a Cell class
    Contains a state

---
+++ Cellular Automata/Assets/Scripts/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell
+{
+    public int state;
+
+    public Cell(int setState)
+    {
+        state = setState;
+    }
+}
