c740c6f26bd36e12b335b4aa631cf433a70b7060
Woods Hong <widewooz@gmail.com>
2020-12-19 16:47:04 +0900

    Create a cellular automaton script, and a rule to follow

---
+++ Cellular Automata/Assets/Scripts/CellularAutomaton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellularAutomaton : MonoBehaviour
+{
+    public Cell[] cells;
+    public Cell[] nextGenCells;
+
+    public Sprite cellSprite;
+
+    protected int historyCount = 0;
+
+    protected int cellCount = 100;
+
+    protected void Initialize()
+    {
+        cells = new Cell[cellCount];
+        nextGenCells = new Cell[cellCount];
+
+        for(int i = 0; i < cellCount; i++)
+        {
+            cells[i] = new Cell(0);
+            nextGenCells[i] = new Cell(0);
+        }
+    }
+
+    protected void CreateHistory()
+    {
+        historyCount++;
+
+        for(int i = 0; i < cellCount; i++)
+        {
+            GameObject cell = new GameObject("Cell" + i);
+            SpriteRenderer sprite = cell.AddComponent<SpriteRenderer>();
+
+            cell.transform.position = new Vector3(i, -historyCount, 0);
+
+            if(cells[i].state == 1)
+            {
+                sprite.color = Color.black;
+            }
+            else if(cells[i].state == 0)
+            {
+                sprite.color = Color.white;
+            }
+
+            sprite.sprite = cellSprite;
+            cell.transform.localScale = Vector3.one * 1;
+        }
+    }
+}
---
+++ Cellular Automata/Assets/Scripts/Rule110.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rule110 : CellularAutomaton
+{
+    Dictionary<(int, int, int), int> rules = new Dictionary<(int, int, int), int>();
+
+    private void Start()
+    {
+        Initialize();
+        cells[98].state = 1;
+        InitializeRules();
+        CreateHistory();
+
+        InvokeRepeating("CheckNewState", 0, 0.3f);
+        InvokeRepeating("CreateHistory", 0.01f, 0.3f);
+    }
+
+    void CheckNewState()
+    {
+        Cell left, center, right;
+
+        for(int i = 1; i < cells.Length - 1; i++)
+        {
+            left = cells[i - 1];
+            center = cells[i];
+            right = cells[i + 1];
+
+            (int, int, int) states =  (left.state, center.state, right.state);
+
+            nextGenCells[i].state = rules[states];
+        }
+
+        for(int i = 0; i < cells.Length; i++)
+        {
+            cells[i].state = nextGenCells[i].state;
+            nextGenCells[i].state = 0;
+        } 
+    }
+    
+    void InitializeRules()
+    {
+        rules.Add((1, 1, 1), 0);
+        rules.Add(( 1, 1, 0 ), 1);
+        rules.Add(( 1, 0, 1 ), 1);
+        rules.Add(( 1, 0, 0 ), 0);
+        rules.Add(( 0, 1, 1 ), 1);
+        rules.Add(( 0, 1, 0 ), 1);
+        rules.Add(( 0, 0, 1 ), 1);
+        rules.Add(( 0, 0, 0 ), 0);
+    }
+}
