3b558d7695df1961ff189513a3f3a9af9eab32f1
Woods Hong <widewooz@gmail.com>
2021-01-09 22:32:35 +0900

    Collision count text

---
+++ Physics Test/Assets/CollsionText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollsionText : MonoBehaviour
+{
+    public Text text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Collisions: " + VelocityCalculation.collision;
+    }
+}
